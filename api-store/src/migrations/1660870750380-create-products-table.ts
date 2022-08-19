adb9fd0066a723bdb5f7b2221b356751a34e57a4
Daniel Vidal <danielsv465@hotmail.com>
2022-08-19 00:30:08 -0300

    Ajuste na migration de tabela produtos

--- api-store/src/migrations/1660870750380-create-products-table.ts
+++ api-store/src/migrations/1660870750380-create-products-table.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateProductsTable1625262588200 implements MigrationInterface {
+export class CreateProductsTable1660870750380 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
