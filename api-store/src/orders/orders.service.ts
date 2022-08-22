32c0f3559da18f84d802b797a09523f7f88f4022
Daniel Vidal <danielsv465@hotmail.com>
2022-08-22 17:52:06 -0300

    Conectando ao gRPC e serviço de pagamento

--- api-store/src/orders/orders.service.ts
+++ api-store/src/orders/orders.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { In, Repository } from 'typeorm';
+import { In, Repository, Connection } from 'typeorm';
 
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
-import { Order } from './entities/order.entity';
+import { Order, OrderStatus } from './entities/order.entity';
 import { Product } from 'src/products/entities/product.entity';
+import { PaymentService } from './payment/payment.service';
 
 @Injectable()
 export class OrdersService {
@@ -13,6 +14,8 @@ export class OrdersService {
     @InjectRepository(Order) private readonly orderRepo: Repository<Order>,
     @InjectRepository(Product)
     private readonly productRepo: Repository<Product>,
+    private paymentService: PaymentService,
+    private connection: Connection,
   ) {}
 
   async create(createOrderDto: CreateOrderDto) {
@@ -26,11 +29,41 @@ export class OrdersService {
       const product = products.find((p) => p.id === item.product_id);
       item.price = product.price;
     });
-    return this.orderRepo.save(order);
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const newOrder = await queryRunner.manager.save(order);
+      await this.paymentService.payment({
+        creditCard: {
+          name: order.credit_card.name,
+          number: order.credit_card.number,
+          expirationMonth: order.credit_card.expiration_month,
+          expirationYear: order.credit_card.expiration_year,
+          cvv: order.credit_card.cvv,
+        },
+        amount: order.total,
+        store: process.env.STORE_NAME,
+        description: `Produtos: ${products.map((p) => p.name).join(', ')}`,
+      });
+      await queryRunner.manager.update(
+        Order,
+        { id: newOrder.id },
+        { status: OrderStatus.Approved },
+      );
+      queryRunner.commitTransaction();
+      return newOrder;
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   findAll() {
-    return `This action returns all orders`;
+    return this.orderRepo.find();
   }
 
   findOne(id: number) {
---
+++ api-store/src/orders/payment/payment.service.ts
@@ -0,0 +1,42 @@
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc, RpcException } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+
+interface PaymentData {
+  creditCard: {
+    name: string;
+    number: string;
+    expirationMonth: number;
+    expirationYear: number;
+    cvv: string;
+  };
+  amount: number;
+  description: string;
+  store: string;
+}
+
+interface PaymentGrpcService {
+  payment(data: PaymentData): Observable<void>;
+}
+
+@Injectable()
+export class PaymentService implements OnModuleInit {
+  private paymentGrpcService: PaymentGrpcService;
+  constructor(@Inject('PAYMENT_PACKAGE') private clientGrpc: ClientGrpc) {}
+
+  onModuleInit() {
+    this.paymentGrpcService =
+      this.clientGrpc.getService<PaymentGrpcService>('PaymentService');
+  }
+
+  async payment(data: PaymentData) {
+    try {
+      return this.paymentGrpcService.payment(data).toPromise();
+    } catch (e) {
+      throw new RpcException({
+        code: e.code,
+        message: e.message,
+      });
+    }
+  }
+}
